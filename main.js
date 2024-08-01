c31b366917cff742b940d29806a3cc77f216a58f
arimura <33759663+arimura@users.noreply.github.com>
2024-08-01 11:21:20 +0900

    ...

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-alert("hoge");
+
+// const element = <h1 title="foo">Hello</h1>
+// const container = document.getElementById("root")
+// ReactDOM.render(element, container)
+
+const element = {
+    type: "h1",
+    props: {
+        title: "foo",
+        children: "Hello",
+    },
+}
+
+const node = document.createElement(element.type)
+node["title"] = element.props.title
+
+const text = document.createTextNode("")
+text["nodeValue"] = element.props.children
+
