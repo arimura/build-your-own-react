53adf92890025c8b72a705517e5819b1b4a00d2e
arimura <33759663+arimura@users.noreply.github.com>
2024-08-01 11:27:36 +0900

    ...

--- main.js
+++ main.js
@@ -11,9 +11,13 @@ const element = {
     },
 }
 
+const container = document.getElementById("root")
+
 const node = document.createElement(element.type)
 node["title"] = element.props.title
 
 const text = document.createTextNode("")
 text["nodeValue"] = element.props.children
 
+node.appendChild(text)
+container.appendChild(node)
