c58a6f16c76dca1641c26b2c94dae9965a9f99d0
arimura <33759663+arimura@users.noreply.github.com>
2024-08-01 11:41:52 +0900

    Add step1

---
+++ step1.js
@@ -0,0 +1,12 @@
+function createElement(type, props, ...children){
+    return {
+        type,
+        props: {
+            ...props,
+            children,
+        },
+    }
+}
+
+console.log(createElement("div"))
+
